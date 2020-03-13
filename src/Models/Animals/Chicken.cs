b6e2e8069e215388e0d89d307fa4d1ef8e71c01a
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-03-13 13:48:04 -0500

    ducks and chickens exist

---
+++ src/Models/Animals/Chicken.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Animals {
+  public class Chicken : IResource, IMeatProducing {
+
+    private Guid _id = Guid.NewGuid ();
+    private double _meatProduced = 1.7;
+
+    private int _eggsProduced = 7;
+
+    private double _feathersProduced = 0.5;
+
+    private string _shortId {
+      get {
+        return this._id.ToString ().Substring (this._id.ToString ().Length - 6);
+      }
+    }
+
+    public double FeedPerDay { get; set; } = 0.9;
+    public string Type { get; } = "Chicken";
+
+    // Methods
+    public void Feed () {
+      Console.WriteLine ($"Chicken {this._shortId} just ate {this.FeedPerDay}kg of feed");
+    }
+
+    public double Butcher () {
+      return _meatProduced;
+    }
+
+    public int Hatch () {
+      return _eggsProduced;
+    }
+
+    public double Pluck () {
+      return _feathersProduced;
+    }
+
+    public override string ToString () {
+      return $"Chicken {this._shortId}. Cluck cluck!";
+    }
+  }
+}
