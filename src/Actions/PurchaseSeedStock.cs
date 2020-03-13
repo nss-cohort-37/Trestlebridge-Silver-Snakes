027c58f6569dc585c9f855f15d01dbb48501e665
Willy <William.g.metcalf@gmail.com>
2020-03-13 14:45:04 -0500

    merging master

---
+++ src/Actions/PurchaseSeedStock.cs
@@ -0,0 +1,41 @@
+using System;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Animals;
+using Trestlebridge.Models.Facilities;
+
+namespace Trestlebridge.Actions {
+    public class PurchaseSeedStock {
+        public static void CollectInput (Farm farm) {
+            Console.WriteLine ("1. Cow");
+            Console.WriteLine ("2. Ostrich");
+            Console.WriteLine ("3. Pig");
+
+            Console.WriteLine ();
+            Console.WriteLine ("What are you buying today?");
+
+            Console.Write ("> ");
+            string choice = Console.ReadLine ();
+
+            switch (Int32.Parse (choice)) {
+                case 1:
+                    ChooseGrazingField.CollectInput (farm, new Cow ());
+                    break;
+                case 2:
+                    ChooseGrazingField.CollectInput (farm, new Ostrich ());
+                    break;
+                case 3:
+                    ChooseGrazingField.CollectInput (farm, new Pig ());
+                    break;
+                case 4:
+                    ChooseGrazingField.CollectInput (farm, new Goat ());
+                    break;
+                case 5:
+                    ChooseGrazingField.CollectInput (farm, new Sheep ());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
