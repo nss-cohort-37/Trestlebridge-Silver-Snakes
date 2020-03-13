63dea58cc1e59b0b03bb4db1a678874134d8d960
Holden Parker <holdenprkr@gmail.com>
2020-03-13 13:41:14 -0500

    now, i promise, you can add all animals to grazing field

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -9,6 +9,9 @@ namespace Trestlebridge.Actions {
         public static void CollectInput (Farm farm) {
             Console.WriteLine ("1. Cow");
             Console.WriteLine ("2. Ostrich");
+            Console.WriteLine ("3. Pig");
+            Console.WriteLine ("4. Goat");
+            Console.WriteLine ("5. Sheep");
 
             Console.WriteLine ();
             Console.WriteLine ("What are you buying today?");
