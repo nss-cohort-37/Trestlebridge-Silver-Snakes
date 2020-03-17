a887b31535b422bcb86a55bf7b53e5aca5d0e327
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-03-17 09:40:55 -0500

    WIP

--- src/Actions/ChooseMeatProcessing.cs
+++ src/Actions/ChooseMeatProcessing.cs
@@ -3,6 +3,7 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Trestlebridge.Actions
 {
@@ -15,11 +16,14 @@ namespace Trestlebridge.Actions
 
       var notEmptyChickenHouses = farm.ChickenHouses.Where(chickenHouse => chickenHouse.Animals.Count > 0).ToList();
 
+      var meatProcessingFacilities = new List<IFacility<I>>();
+
       for (int i = 0; i < notEmptyGrazingFields.Count; i++)
       {
-        Console.Write($"{i + 1}. Grazing Field {notEmptyGrazingFields[i].AnimalCount()} ");
-        notEmptyGrazingFields[i].AnimalTypeCount();
-        Console.WriteLine();
+        // Console.Write($"{i + 1}. Grazing Field {notEmptyGrazingFields[i].AnimalCount()} ");
+        // notEmptyGrazingFields[i].AnimalTypeCount();
+        // Console.WriteLine();
+        meatProcessingFacilities.Add(notEmptyGrazingFields[i]);
       }
 
       for (int i = 0; i < notEmptyChickenHouses.Count; i++)
