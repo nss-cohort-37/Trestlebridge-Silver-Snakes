904e8a7387f171e7c6a2610009b39a5d92d27b5b
Willy <William.g.metcalf@gmail.com>
2020-03-17 11:17:17 -0500

    working on multiple chickens

--- src/Actions/ChooseChickenHouse.cs
+++ src/Actions/ChooseChickenHouse.cs
@@ -1,62 +1,94 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 
-namespace Trestlebridge.Actions {
-  public class ChooseChickenHouse {
-    public static void CollectInput (Farm farm, Chicken animal) {
-      Utils.Clear ();
-
-      var availableChickenHouses = farm.ChickenHouses.Where(chickenHome => chickenHome.Animals.Count < chickenHome.Capacity).ToList();
-
-      if (availableChickenHouses.Count == 0)
-      {
-          Console.WriteLine("There are no available chicken houses. \nPress return to go back to the main menu");
-          Console.ReadLine();
-      }
-      else
-      {
-        for (int i = 0; i < availableChickenHouses.Count; i++) {
-          Console.Write ($"{i + 1}. Chicken House {availableChickenHouses[i].AnimalCount()} ");
-          Console.WriteLine ();
-        }
-
-        Console.WriteLine ();
-
-        // How can I output the type of animal chosen here?
-        while(true)
+namespace Trestlebridge.Actions
+{
+    public class ChooseChickenHouse
+    {
+        public static void CollectInput(Farm farm, List<Chicken> animals)
         {
-          Console.WriteLine ($"Place the chicken where? or hit return to exit");
-          Console.Write ("> ");
-            try
+            Utils.Clear();
+
+            var availableChickenHouses = farm.ChickenHouses.Where(chickenHome => chickenHome.Animals.Count < chickenHome.Capacity).ToList();
+
+            if (availableChickenHouses.Count == 0)
             {
-              var choice = Console.ReadLine ();
-              if (String.IsNullOrEmpty(choice))
-              {
-                break;
-              }
-              else
-              {
-                availableChickenHouses[Int32.Parse (choice) - 1].AddResource (animal);
-                break;
-              }
+                Console.WriteLine("There are no available chicken houses. \nPress return to go back to the main menu");
+                Console.ReadLine();
             }
-            catch
+            else
             {
-              Console.WriteLine("Please enter a valid index range");
+                for (int i = 0; i < availableChickenHouses.Count; i++)
+                {
+                    Console.Write($"{i + 1}. Chicken House {availableChickenHouses[i].AnimalCount()} ");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+                // How can I output the type of animal chosen here?
+                while (true)
+                {
+                    Console.WriteLine($"Place the chickens where? \nYou have {animals.Count} to place\nor hit return to exit");
+                    Console.Write("> ");
+                    try
+                    {
+                        var choice = Console.ReadLine();
+                        if (String.IsNullOrEmpty(choice))
+                        {
+                            break;
+                        }
+                        else
+                        {
+
+                            while (true)
+                            {
+                                Console.WriteLine("How many would you like to add to this house?");
+                                try
+                                {
+                                    int amount = int.Parse(Console.ReadLine());
+                                    var initialCount = animals.Count;
+                                    for (int i = 0; i < amount; i++)
+                                    {
+                                        availableChickenHouses[Int32.Parse(choice) - 1].AddResource(animals[i]);
+                                        animals.Remove(animals[i]);
+                                    }
+
+
+
+                                }
+                                catch
+                                {
+
+                                }
+                                break;
+                            }
+
+                            if (animals.Count == 0)
+                            {
+                                break;
+                            }
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a valid index range");
+                    }
+                }
             }
+
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+
         }
-      }
-
-
-      /*
-          Couldn't get this to work. Can you?
-          Stretch goal. Only if the app is fully functional.
-       */
-      // farm.PurchaseResource<IGrazing>(animal, choice);
-
     }
-  }
 }
--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
@@ -6,74 +7,91 @@ using Trestlebridge.Models.Facilities;
 
 namespace Trestlebridge.Actions
 {
-  public class PurchaseStock
-  {
-    public static void CollectInput(Farm farm)
+    public class PurchaseStock
     {
-      Console.WriteLine("1. Cow");
-      Console.WriteLine("2. Ostrich");
-      Console.WriteLine("3. Pig");
-      Console.WriteLine("4. Goat");
-      Console.WriteLine("5. Sheep");
-      Console.WriteLine("6. Duck");
-      Console.WriteLine("7. Chicken");
-
-      Console.WriteLine();
-
-      while (true)
-      {
-        Console.WriteLine("What are you buying today? Hit return to exit");
-        Console.Write("> ");
-        try
+        public static void CollectInput(Farm farm)
         {
-          string choice = Console.ReadLine();
-          if (String.IsNullOrEmpty(choice))
-          {
-            break;
-          }
-          else
-          {
-            if (Int32.Parse(choice) < 1 || Int32.Parse(choice) > 7)
+
+            var ChickenList = new List<Chicken>();
+
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Ostrich");
+            Console.WriteLine("3. Pig");
+            Console.WriteLine("4. Goat");
+            Console.WriteLine("5. Sheep");
+            Console.WriteLine("6. Duck");
+            Console.WriteLine("7. Chicken");
+
+            Console.WriteLine();
+
+            while (true)
             {
-              Console.WriteLine("Please enter a valid index range");
+                Console.WriteLine("What are you buying today? Hit return to exit");
+                Console.Write("> ");
+                try
+                {
+                    string choice = Console.ReadLine();
+                    if (String.IsNullOrEmpty(choice))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (Int32.Parse(choice) < 1 || Int32.Parse(choice) > 7)
+                        {
+                            Console.WriteLine("Please enter a valid index range");
+                        }
+                        else
+                        {
+                            switch (Int32.Parse(choice))
+                            {
+                                case 1:
+                                    ChooseGrazingField.CollectInput(farm, new Cow());
+                                    break;
+                                case 2:
+                                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                                    break;
+                                case 3:
+                                    ChooseGrazingField.CollectInput(farm, new Pig());
+                                    break;
+                                case 4:
+                                    ChooseGrazingField.CollectInput(farm, new Goat());
+                                    break;
+                                case 5:
+                                    ChooseGrazingField.CollectInput(farm, new Sheep());
+                                    break;
+                                case 6:
+                                    ChooseDuckHouse.CollectInput(farm, new Duck());
+                                    break;
+                                case 7:
+                                    Console.WriteLine("How many Chickens would you like to buy?");
+                                    try
+                                    {
+
+                                        int amount = int.Parse(Console.ReadLine());
+                                        for (int i = 0; i < amount; i++)
+                                        {
+                                            ChickenList.Add(new Chicken());
+                                        }
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                    ChooseChickenHouse.CollectInput(farm, ChickenList);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid index range");
+                }
             }
-            else
-            {
-              switch (Int32.Parse(choice))
-              {
-                case 1:
-                  ChooseGrazingField.CollectInput(farm, new Cow());
-                  break;
-                case 2:
-                  ChooseGrazingField.CollectInput(farm, new Ostrich());
-                  break;
-                case 3:
-                  ChooseGrazingField.CollectInput(farm, new Pig());
-                  break;
-                case 4:
-                  ChooseGrazingField.CollectInput(farm, new Goat());
-                  break;
-                case 5:
-                  ChooseGrazingField.CollectInput(farm, new Sheep());
-                  break;
-                case 6:
-                  ChooseDuckHouse.CollectInput(farm, new Duck());
-                  break;
-                case 7:
-                  ChooseChickenHouse.CollectInput(farm, new Chicken());
-                  break;
-                default:
-                  break;
-              }
-              break;
-            }
-          }
         }
-        catch
-        {
-          Console.WriteLine("Please enter a valid index range");
-        }
-      }
     }
-  }
 }
